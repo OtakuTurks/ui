a4861a9b27c2cd60335aad10395459afb38795bd
ewgsta <159681870+ewgsta@users.noreply.github.com>
2026-05-17 04:28:34 +0300

    feat: add OtDivider component

---
+++ src/components/OtDivider.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+  vertical: { type: Boolean, default: false },
+  dashed: { type: Boolean, default: false }
+})
+</script>
+<template>
+  <div
+    class="ot-divider"
+    :class="{ 'ot-divider--vertical': vertical, 'ot-divider--dashed': dashed }"
+  ></div>
+</template>
+<style scoped>
+.ot-divider {
+  background-color: var(--ot-gray-300, #4b5563);
+  margin: var(--ot-spacing-md, 16px) 0;
+  height: 1px;
+  width: 100%;
+}
+.ot-divider--vertical {
+  width: 1px;
+  height: auto;
+  margin: 0 var(--ot-spacing-md, 16px);
+  display: inline-block;
+}
+.ot-divider--dashed {
+  background-color: transparent;
+  border-top: 1px dashed var(--ot-gray-300, #4b5563);
+}
+.ot-divider--vertical.ot-divider--dashed {
+  border-top: none;
+  border-left: 1px dashed var(--ot-gray-300, #4b5563);
+}
+</style>
